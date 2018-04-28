b0a2aa94e6a17b9a638713befb01bdc30458bffc
Krasimir Zahariev <k.zahariew@abv.bg>
2018-04-28 09:40:49 +0300

    added some functionality to Attraction class

--- src/park/funzone/Attraction.java
+++ src/park/funzone/Attraction.java
@@ -5,4 +5,35 @@ public class Attraction {
     private AttractionType type;
     private int availableSeats;
 
+    public Attraction(String name, AttractionType type, int availableSeats) {
+        this.name = name;
+        this.type = type;
+        this.availableSeats = availableSeats;
+    }
+
+    public void visitAttraction(String userName, int userAge) {
+        boolean isAllowed = isAttractionAllowed(userAge, type);
+        if (isAllowed) {
+            System.out.println(userName + " is having good time on " + name);
+            // TODO : check ticket
+        } else {
+            System.out.println("We are sorry, but " + name + " is considered dangerous.");
+        }
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public AttractionType getType() {
+        return type;
+    }
+
+    public int getAvailableSeats() {
+        return availableSeats;
+    }
+
+    private boolean isAttractionAllowed(int userAge, AttractionType type) {
+        return (userAge >= 15 && userAge <= 65) || type != AttractionType.DANGER;
+    }
 }
