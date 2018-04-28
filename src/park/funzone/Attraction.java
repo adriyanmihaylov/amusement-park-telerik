7fdd7883745cd2cce8f6b96a0c07d8515ca0d92b
Krasimir Zahariev <k.zahariew@abv.bg>
2018-04-28 09:10:17 +0300

    added new enum Attraction type and made some chnages to the Attraction class

--- src/park/funzone/Attraction.java
+++ src/park/funzone/Attraction.java
@@ -1,5 +1,8 @@
 package park.funzone;
 
 public class Attraction {
+    private String name;
+    private AttractionType type;
+    private int availableSeats;
 
 }
---
+++ src/park/funzone/AttractionType.java
@@ -0,0 +1,5 @@
+package park.funzone;
+
+public enum AttractionType {
+    SAFE, DANGER
+}
