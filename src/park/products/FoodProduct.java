1d88aa79e271dc642cce52eac459baa125a78171
Echolz <elizaremanuilov@gmail.com>
2018-04-29 16:02:10 +0300

    fixed typo

--- src/park/products/FoodProduct.java
+++ src/park/products/FoodProduct.java
@@ -4,14 +4,14 @@ import park.interfaces.IConsumable;
 
 public abstract class FoodProduct extends Product implements IConsumable {
 
-    String exparationDate;
+    String expirationDate;
 
     public FoodProduct(String name, double price, String expirationDate) {
         super(name, price);
-        this.exparationDate = expirationDate;
+        this.expirationDate = expirationDate;
     }
 
     public String getExparationDate() {
-        return exparationDate;
+        return expirationDate;
     }
 }
